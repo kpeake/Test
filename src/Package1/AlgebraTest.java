bbf8f27e0a6112fa848f09e0a209fbcf2e2dc485
kpeakeO2 <kerry.peake@o2.com>
2015-03-18 15:41:53 +0000

    Delete me (3)

--- src/Package1/AlgebraTest.java
+++ src/Package1/AlgebraTest.java
@@ -12,7 +12,7 @@ public class AlgebraTest {
 		Algebra algebra = new Algebra();
 		
 		assertEquals("2 + 4 must equal 6", 6, algebra.addMeUp(2,  4));
-		//assertEquals("2 + 4 must equal 6", 5, algebra.addMeUp(2,  4));
+		assertEquals("2 + 4 must equal 6", 5, algebra.addMeUp(2,  4));
 		
 		//fail("Not yet implemented");
 	}
